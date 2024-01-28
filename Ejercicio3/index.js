b101e58a5c0fc7a0f63155ea7628893cb0636365
lauraesri <156214164+lauraesri@users.noreply.github.com>
2024-01-28 19:55:53 +0100

    E3

---
+++ Ejercicio3/index.js
@@ -0,0 +1,28 @@
+const data = ['hola', 2, 5, 'adios'];
+
+// Determina cuál de los dos números que hay en el array es mayor
+
+const comp = data[1]>data[2]
+console.log ('¿El valor '+ data[1] + ' es > que '+ data[2] + '? ' + comp)
+
+//Determina el resultado de las cinco operaciones numéricas
+
+//Suma (+)
+const suma = data[1] + data[2]
+console.log('La suma es ' + suma)
+
+//Resta (-)
+const resta = data[1] - data[2]
+console.log('La resta es ' + resta)
+
+//Cociente (/)
+const cociente = data[1] / data[2]
+console.log('El cociente es ' + cociente)
+
+//Producto (*)
+const producto = data[1] * data[2]
+console.log('El producto es ' + producto)
+
+//Resto (%)
+const resto = data[1] % data[2]
+console.log('El resto es ' + resto)
