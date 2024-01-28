ec41dcf60cb745c89d53b493cc0cc9dfebae003a
lauraesri <156214164+lauraesri@users.noreply.github.com>
2024-01-28 18:45:50 +0100

    E1

--- Ejercicio1/index.js
+++ Ejercicio1/index.js
@@ -1,7 +1,17 @@
 // Calculo del volumen
 const volumen = 16*23*13;
-console.log('volumen (cm3)', volumen)
+console.log('volumen (cm3)', volumen);
 
 // Calculo del area
-const area = 13*23 + 2*23*16
-console.log('area (cm2)', area)
+const area = 13*23 + 2*23*16;
+console.log('area (cm2)', area);
+
+// Generalizar la operación
+const largo = 16;
+const ancho = 23;
+const alto = 13;
+
+const volumen2 = largo * ancho * alto;
+const area2 = alto * ancho + 2 * ancho * largo;
+console.log('volumen (cm3)', volumen2);
+console.log('area (cm2)', area2);
