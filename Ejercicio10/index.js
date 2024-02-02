a94a65987a14e12d062287b39366e81d3742bf0d
lauraesri <156214164+lauraesri@users.noreply.github.com>
2024-02-02 21:45:01 +0100

    Ejercicio 10

---
+++ Ejercicio10/index.js
@@ -0,0 +1,11 @@
+function paresImpares (numero){
+    if (numero % 2 === 0){
+        texto = 'Es un número par';
+      }
+    else {
+        texto = 'Es un número impar';
+      }
+
+    return texto;   
+}
+
